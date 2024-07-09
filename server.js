51d236c7f0edca5db5bb08edc8be45be040bf869
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-07-09 16:42:45 -0500

    adding data / routes / server

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // import posts from './routes/posts.js';
 // import logger from './middleware/logger.js';
 // import errorHandler from './middleware/error.js';
 // import notFound from './middleware/notFound.js';
 const app = express();
 const port = process.env.PORT || 8080;
-// const fs = require('fs');
+
+app.use(express.json());
+
+// Read posts from file
+const readPostsFromFile = () => {
+  const data = fs.readFileSync(path.join(__dirname, 'data', 'posts.json'));
+  return JSON.parse(data);
+};
+
+let posts = readPostsFromFile();
 
 // TEMPLATE ENGINE
 
